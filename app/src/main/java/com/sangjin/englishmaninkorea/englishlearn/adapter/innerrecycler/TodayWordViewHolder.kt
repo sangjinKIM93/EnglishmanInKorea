efa4b75233bcd4e2b934ce97fa933ac9e554229d
sangjinKIM93 <larger611@gmail.com>
2020-06-11 12:09:54 +0900

    TodayWordViewHolder.kt 예외처리 및 리팩토링
    단어장에 저장된 단어가 4개 이하인 경우에 대한 예외 처리

--- app/src/main/java/com/sangjin/englishmaninkorea/englishlearn/adapter/innerrecycler/TodayWordViewHolder.kt
+++ app/src/main/java/com/sangjin/englishmaninkorea/englishlearn/adapter/innerrecycler/TodayWordViewHolder.kt
@@ -3,36 +3,63 @@ package com.sangjin.englishmaninkorea.englishlearn.adapter.innerrecycler
 import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.sangjin.englishmaninkorea.englishlearn.data.model.TodayWord
 import com.sangjin.englishmaninkorea.vocabularylist.data.Voca
-import kotlinx.android.synthetic.main.item_recycler_recyler.view.*
 import kotlinx.android.synthetic.main.item_recycler_recyler.view.rv_item
 import kotlinx.android.synthetic.main.item_today_voca_recycler.view.*
 
-class TodayWordViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView){
+class TodayWordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bindView(todayWords: List<Voca>){
+    fun bindView(todayWords: List<Voca>) {
 
         val todayWordAdapter = TodayWordAdapter()
-        todayWordAdapter.submitList(todayWords.subList(0,4))
-
         itemView.rv_item.adapter = todayWordAdapter
         itemView.rv_item.layoutManager = GridLayoutManager(itemView.context, 2)
 
+        //** 등록된 단어가 4개 이하인 경우와 4개 이상인 경우 나누기
+        if (todayWords.size > 4) {
 
-        var stateFirst = true
+            todayWordAdapter.submitList(todayWords.subList(0, 4))
+            itemView.btn_other_word.visibility = View.VISIBLE
 
-        itemView.btn_other_word.setOnClickListener {
-            if(stateFirst == true){
-                todayWordAdapter.submitList(todayWords.subList(4,8))
-                itemView.btn_other_word.text = "다른 단어 보기 2/2"
-                stateFirst = false
-            } else {
-                todayWordAdapter.submitList(todayWords.subList(0,4))
-                itemView.btn_other_word.text = "다른 단어 보기 1/2"
-                stateFirst = true
-            }
+            //단어 전환 버튼 클릭 리스너
+            wordChangeBtnListener(todayWords, todayWordAdapter)
+
+        } else {
+
+            todayWordAdapter.submitList(todayWords.subList(0, todayWords.lastIndex+1))
+            itemView.btn_other_word.visibility = View.GONE
+
         }
 
+    }
+
+    private fun wordChangeBtnListener(
+        todayWords: List<Voca>,
+        todayWordAdapter: TodayWordAdapter
+    ) {
+        var isFirst = true  //"다른 단어 보기 1/2" 일때 true
+
+        itemView.btn_other_word.setOnClickListener {
+
+            if (isFirst) {
+
+                //** 단어가 8개 보다 많은 경우 8개 까지만 자르기
+                if (todayWords.size > 8) {
+                    todayWordAdapter.submitList(todayWords.subList(4, 8))
+                } else {
+                    todayWordAdapter.submitList(todayWords.subList(4, todayWords.lastIndex + 1))
+                }
+
+                itemView.btn_other_word.text = "다른 단어 보기 2/2"
+                isFirst = false
+
+            } else {
+                todayWordAdapter.submitList(todayWords.subList(0, 4))
+
+                itemView.btn_other_word.text = "다른 단어 보기 1/2"
+                isFirst = true
+            }
+
+        }
     }
 }
